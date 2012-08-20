99c12bdc733b4da3956f81a646237e78bae5cc6d
m.kulebyakin <kulebyakin@gmail.com>
2012-08-20 14:20:25 +0400

    try

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -66,7 +66,7 @@ class Configuration implements ConfigurationInterface
                     ->children()
 
 
-                        ->requiresAtLeastOneElement()
+                      //  ->requiresAtLeastOneElement()
                           ->arrayNode('index')
                                ->useAttributeAsKey('key')
                                ->prototype('array')
